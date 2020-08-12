4fdee2e9f62157a2122389f21cde26ac4ecd3065
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-08-12 14:58:30 -0300

    Criado função de pular e gravity scale do Player modificado pra 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float movementInputDirection; 
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
+    private float _movementInputDirection; 
+    private bool _isFacingRight = true;
     public float movementSpeed = 10.0f;
+    public float jumpForce = 16.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
     
     // Update is called once per frame
@@ -19,19 +21,51 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CheckInput();
+        CheckMovementDirection();
     }   
 
     private void FixedUpdate()
     {
         ApplyMovement();
     }
+
+    private void CheckMovementDirection()
+    {
+        if(_isFacingRight && _movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if(!_isFacingRight && _movementInputDirection > 0)
+        {
+            Flip();
+        }
+    }
+
     private void CheckInput()
     {
-        movementInputDirection = Input.GetAxisRaw("Horizontal");
+        _movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
     }
 
     private void ApplyMovement()
     {
-        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        _rb.velocity = new Vector2(movementSpeed * _movementInputDirection, _rb.velocity.y);
     }
+
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+
 }
