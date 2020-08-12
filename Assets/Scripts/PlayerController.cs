607f566bc2995e5b8227837ae3ab066091e7eba4
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-08-12 14:30:42 -0300

    Adicionado movimentação horizontal no Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float movementInputDirection; 
+    private Rigidbody2D rb;
+    public float movementSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
+    
     // Update is called once per frame
+
     void Update()
     {
-        
+        CheckInput();
+    }   
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
     }
 }
